2c2f3028310bd9b1d25d917953d2a08d57391678
brijmohanG <brijmohangautam449@gmail.com>
2025-09-08 00:52:51 +0530

    core setting

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use('/api', appRoutes);
-app.use(cors());
+
 
 
 // Test route
